163bd661d43fafc05c289e5f85743c846a521566
Kolko Valentin <valiant9191@gmail.com>
2023-05-23 21:18:42 +0300

    add: search form on job list;

--- src/components/pages/searchJobList/searchJobList.tsx
+++ src/components/pages/searchJobList/searchJobList.tsx
@@ -1,11 +1,41 @@
+import { useForm } from '@mantine/form';
+import { Input, Button, Flex, Box, Container } from '@mantine/core';
+
+import { VacancyShortComponent } from "components/vacancyShortComponent";
+import styles from './searcJobList.module.css'
 
 
 function SearchJobList() {
+    const form = useForm({
+        initialValues: {
+            search: '',
+        },
+    });
 
     return (
-        <div>
-            searchJobList page
-        </div>
+        <Container className={styles.container} >
+            <Box p='2rem' mx="auto" mt='1.2rem'>
+                <form className={styles.form} onSubmit={form.onSubmit((values) => console.log(values))}>
+                    <Flex className={styles.search} >
+                        <Input
+                            className={styles.inputSearch}
+                            placeholder="Введите название вакансии"
+                            /* eslint-disable-next-line react/jsx-props-no-spreading */
+                            {...form.getInputProps('search')}
+                        />
+
+                        <Button className={styles.submitBtn} type="submit">Поиск</Button>
+                    </Flex>
+                </form>
+            </Box>
+            <div>
+                <ul>
+                    <li><VacancyShortComponent /></li>
+                    <li><VacancyShortComponent /></li>
+                    <li><VacancyShortComponent /></li>
+                </ul>
+            </div>
+        </Container >
     )
 }
 export { SearchJobList };
